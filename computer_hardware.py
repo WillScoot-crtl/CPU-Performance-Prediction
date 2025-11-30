816a1bd1305f364d6e70073b6fb6779d82e97486
unknown <WebbWilliam06@gmail.com>
2025-11-30 16:10:22 -0600

    Completed Regression Model
    This version of the linear regression model implements backwards elimination to create a multi linear regression model.
    
    Overall, prediction became more accurate compared to the previous version

--- computer_hardware.py
+++ computer_hardware.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler 
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import FunctionTransformer
+import statsmodels.api as sm
+
+
 
 
 def fcpu():
@@ -16,11 +20,30 @@ def fcpu():
     df = pd.read_csv("machine.data", names=cols)
     x = df.iloc[:,2:-2].values
     y = df.iloc[:,8].values
+    return x, y
 
-    return df, x, y
+### optimize the model with backwards elimination to select that most significant predictors
+
+def backwards_elimination(x, y, sL = 0.05):
+
+
+    x_opt = x.copy()
+    
+    num_var = x.shape[1]
+    for _ in range(num_var):
+        ols_model = sm.OLS(y, sm.add_constant(x_opt)).fit()
+        max_pvalue = max(ols_model.pvalues[1:])
+        if max_pvalue > sL:
+            max_p_index = np.argmax(ols_model.pvalues[1:])
+            x_opt = np.delete(x_opt, max_p_index, axis=1)
+        else:
+            break
+
+
+    return x_opt, ols_model
 
 def training_prep():
-    df, x, y = fcpu()
+    x, y = fcpu()
 
     x_train, x_test, y_train, y_test = train_test_split(x ,y,test_size =  0.2, random_state = 0)
     return x_train, x_test, y_train, y_test
@@ -28,18 +51,39 @@ def training_prep():
 def standardizing():
     x_train, x_test, y_train, y_test = training_prep()
 
+
     sc_x  = StandardScaler()
     x_train_scaled = sc_x.fit_transform(x_train)
     x_test_scaled = sc_x.transform(x_test)
 
     return x_train_scaled, x_test_scaled, y_train, y_test
 
-def regression_model():
+def scaled_x():
     x_train, x_test, y_train, y_test = standardizing()
 
+    # backwards elimination implementation
+    x_full = np.vstack((x_train, x_test))
+    y_full = np.concatenate((y_train, y_test))
+
+    x_full_const = np.append(arr=np.ones((x_full.shape[0], 1)), values=x_full, axis=1)
+    x_reduced, model = backwards_elimination(x_full_const, y_full, sL = 0.05)
+
+    # reduced matrix into train/tet
+    n_train = x_train.shape[0]
+
+    x_reduced_train = x_reduced[:n_train, :]
+    x_reduced_test = x_reduced[n_train:, :]
+
+    return x_reduced_train, x_reduced_test, y_train, y_test
+
+def regression_model():
+    x_reduced_train, x_reduced_test, y_train, y_test = scaled_x()
+
+
     regressor = LinearRegression()
-    regressor.fit(x_train, y_train)
-    y_pred = regressor.predict(x_test)
+    regressor.fit(x_reduced_train, y_train)
+
+    y_pred = regressor.predict(x_reduced_test)
 
     return y_pred, y_test
 
@@ -48,9 +92,38 @@ def data_comparision():
 
     df1 = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
     df1.plot(kind='bar')
+    plt.xlabel("Sample")
+    plt.ylabel("Published Relative perforamnce")
+    plt.title("Supervised Regression Model of CPU Performance")
     plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
     plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
     plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
